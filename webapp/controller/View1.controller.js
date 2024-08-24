5ff9d0bb26f485d83c16212bbca0c2ac45276b1b
Kaushiki Trivedi <kaushiki.trivedi@accenture.com>
2024-08-24 07:00:46 +0000

    Adding SAP Fiori Button Page Code

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,38 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, toast) {
+        "use strict";
+
+        return Controller.extend("com.login.lkm.loginpagedemo.controller.View1", {
+            onInit: function () {
+
+            },
+            onPress:function()
+	{
+		var v1 = this.getView().byId("name").getValue();
+		
+		var v2 = this.getView().byId("pwd").getValue();  
+		
+		if((v1==="") || (v2===""))
+		{
+			toast.show("Please enter all Values!");
+		}
+		else
+		{
+			this.getView().byId("vals").setText("Welcome,"+ v1);
+			toast.show("Welcome," + this.getView().byId("name").getValue());
+		}
+	},
+	onClear:function()
+	{
+		this.getView().byId("name").setValue("");
+		this.getView().byId("pwd").setValue("");
+	}
+        });
+    });
+
